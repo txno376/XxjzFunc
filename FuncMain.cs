c5ecaa59f63622170fa32d24a1df5b5f206e183e
TAN <txtxeon3376@gmail.com>
2025-02-04 17:56:15 +0800

    添加项目文件。

---
+++ FuncMain.cs
@@ -0,0 +1,148 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using config.export.excel;
+using GS;
+using GS.Event;
+using HarmonyLib;
+using Il2CppSystem.Collections.Generic;
+using System;
+
+namespace XxjzFunc
+{
+    [BepInPlugin("XxjzFuncMod", "XxjzFunc", "1.0.0")]
+    public class FuncMain : BasePlugin
+    {
+        private static ConfigEntry<int> 境界数;
+        private static Harmony harmony;
+        public override void Load()
+        {
+            FuncMain.境界数 = base.Config.Bind<int>("XxjzFunc", "境界数", 71, "境界数量");
+            FuncMain.harmony = Harmony.CreateAndPatchAll(typeof(FuncMain), null);
+            base.Log.LogError("FuncMain OK!");
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GSActor), "ExecFunc")]
+        public static bool Ascension(ref int funcType, ref int param1, ref int param2, ref int param3, ref EventNode context, ref GSActor __instance)
+        {
+            Console.WriteLine($"{funcType}---{param1}---{param2}---{param3}--");
+            switch (funcType)
+            {
+                case 64: //改年龄
+                    {
+                        Funccase.ChangeAge(__instance, param1, param2, param3);
+                        return false;
+                    }
+                case 65: //改血脉
+                    {
+                        __instance.ChangeBloods(param1, param2);
+                        return false;
+                    }
+                case 66: //改境界
+                    {
+                        if (param1 == 0)
+                        {
+                            __instance.ChangeRealmEffect(param2);
+                        }
+                        if (param1 == 1)
+                        {
+                            __instance.ChangeRealmEffect(-param2);
+                        }
+                        return false;
+                    }
+                case 67: //通过血脉获得丹药
+                    {
+                        Funccase.ByAttGetItem(__instance, param1, param2, param3, context);
+                        return false;
+                    }
+                case 68: //获得天赋
+                    {
+                        RoleTalent roleTalent = RoleTalentSelectHelper.AddRandomTalent(__instance, param1);
+                        if (roleTalent != null)
+                        {
+                            __instance.AddTalent(roleTalent, true);
+                        }
+                        return false;
+                    }
+                case 69: //获得血脉天赋
+                    {
+                        RoleTalent roleTalent = RoleTalent.Find(param1);
+                        if (roleTalent != null)
+                        {
+                            __instance.AddBloodTalent(roleTalent, true);
+                        }
+                        return false;
+                    }
+            }
+            return true;
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(RoleState), "InitRealms")]
+        public static bool InitRealmsMod()
+        {
+            Console.WriteLine("--InitRealms------------------------------------");
+            int valuecount = FuncMain.境界数.Value;
+            bool flag = RoleState.realms == null;
+            if (flag)
+            {
+                RoleState.realms = new RoleState[valuecount];
+                foreach (KeyValuePair<int, RoleState> keyValuePair in RoleState.configs)
+                {
+                    bool flag2 = keyValuePair.Key >= 0 && keyValuePair.Key < valuecount;
+                    if (flag2)
+                    {
+                        RoleState.realms[keyValuePair.Key] = keyValuePair.Value;
+                    }
+                }
+            }
+            return false;
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(RoleState), "GetRealm")]
+        public static void GetRealm(ref int realm, ref RoleState __result)
+        {
+            int valuecount = FuncMain.境界数.Value;
+            if (realm > 70)
+            {
+                Console.WriteLine(realm + "--GetRealm--70+---------------------------------");
+                bool flag = RoleState.realms == null;
+                if (flag)
+                {
+                    RoleState.InitRealms();
+                }
+                __result = (realm < valuecount) ? RoleState.realms[realm] : null;
+            }
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GSItemMgr), "UseItem")]
+        public static void UseItem(ref bool __result, ref GSItem item, ref GSActor p, ref GSItemMgr __instance)
+        {
+            if (__result && item.proto.createSkillId < 7000000)
+            {
+                Console.WriteLine(item.proto.createSkillId + "EventID");
+                EventTmpl eventTmpl = EventTmpl.Find(item.proto.createSkillId);
+                if (eventTmpl == null)
+                {
+                    Console.WriteLine("Not Find EventID");
+                }
+                else
+                {
+                    EventNode context = EventNode.Create(eventTmpl, EventNode.AquireFromPool());
+                    int[] execFunc = eventTmpl.execFunc;
+                    bool flag13 = execFunc != null && execFunc.Length != 0;
+                    if (flag13)
+                    {
+                        int j = 0;
+                        int num6 = execFunc.Length;
+                        while (j < num6)
+                        {
+                            p.ExecFunc(execFunc[j], execFunc[j + 1], execFunc[j + 2], execFunc[j + 3], context);
+                            j += 4;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Funccase.cs
@@ -0,0 +1,156 @@
+﻿using config.export.excel;
+using GS;
+using GS.Event;
+using Il2CppSystem.Collections.Generic;
+using System;
+
+namespace XxjzFunc
+{
+    public class Funccase
+    {
+        public static void ChangeAge(GSActor gSActor, int param1, int param2, int param3) 
+        {
+            Random random = new Random();
+            int num0 = random.Next(param2, param3);
+            int value = gSActor.age;
+            int cache;
+            switch (param1)
+            {
+                case 0:
+                    {
+                        cache = (value + num0 > 0) ? (value + num0) : 0;
+                        if (cache > 0)
+                        {
+                            gSActor.age = cache;
+                        }
+                        else
+                        {
+                            gSActor.OnDying(6);
+                        }
+                        break;
+                    }
+                case 1:
+                    {
+                        cache = (value + param2 > 0) ? (value + param2) : 0;
+                        if (cache > 0)
+                        {
+                            gSActor.age = cache;
+                        }
+                        else
+                        {
+                            gSActor.OnDying(6);
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        cache = num0 > 0 ? num0 : 0;
+                        if (cache > 0)
+                        {
+                            gSActor.age = cache;
+                        }
+                        else
+                        {
+                            gSActor.OnDying(6);
+                        }
+                        break;
+                    }
+                case 3:
+                    {
+                        cache = param2 > 0 ? param2 : 0;
+                        if (cache > 0)
+                        {
+                            gSActor.age = cache;
+                        }
+                        else
+                        {
+                            gSActor.OnDying(6);
+                        }
+                        break;
+                    }
+            }
+        }
+
+        public static void ByAttGetItem(GSActor gSActor, int param1, int param2, int param3, EventNode context)
+        {
+            switch (param1) 
+            {
+                case 0: //通过血脉获取
+                    {
+                        int bloodid = 0, bloodvalue = 0;
+                        foreach (KeyValuePair<int, int> keyValuePair in gSActor.bloods)
+                        {
+                            bool flag = keyValuePair.Value > bloodvalue;
+                            if (flag)
+                            {
+                                bloodid = keyValuePair.Key;
+                                bloodvalue = keyValuePair.Value;
+                            }
+                        }
+                        if (bloodvalue > 0)
+                        {
+                            int value = bloodvalue - param2 * 10;
+                            if (value > 0)
+                            {
+                                gSActor.bloods[bloodid] = value;
+                                int itemid = param3 + bloodid * 10 + param2;
+                                gSActor.force.itemMgr.AddNewItem(itemid, 1);
+                            }
+                            else
+                            {
+                                gSActor.bloods.Remove(bloodid);
+                            }
+                        }
+                        break;
+                    }
+                case 1: //通过年龄获取
+                    {
+                        int value = gSActor.age - param2;
+                        if (value > 0)
+                        {
+                            gSActor.age = value;
+                        }
+                        else 
+                        {
+                            gSActor.OnDying(6);
+                        }
+                        gSActor.force.itemMgr.AddNewItem(param3, 1);
+                        break;
+                    }
+                case 2: //通过境界获取
+                    {
+                        int value = gSActor.realm - param2;
+                        if (value > 0)
+                        {
+                            gSActor.ChangeRealmEffect(-param2);
+                        }
+                        else
+                        {
+                            gSActor.OnDying(6);
+                        }
+                        gSActor.force.itemMgr.AddNewItem(param3, 1);
+                        break;
+                    }
+                case 3: //通过天赋获取
+                    {
+                        RoleTalent rtalent = new RoleTalent();
+                        int value = 1;
+                        foreach (RoleTalent roleTalent in gSActor.talents)
+                        {
+                            value = roleTalent.id;
+                            if (value == context.eventTmpl.fourthValue) 
+                            {
+                                rtalent = roleTalent;
+                            }
+                        }
+                        if (rtalent != null)
+                        {
+                            gSActor.DelTalent(rtalent);
+                            gSActor.force.itemMgr.AddNewItem(param3, 1);
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
